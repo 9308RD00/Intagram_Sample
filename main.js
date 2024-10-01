d14ba26f991fb115ca4ebf0f05e3b5ff8ec7e0ee
Redando Dieujuste <101023464+9308RD00@users.noreply.github.com>
2024-10-01 04:40:49 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onresize = screen;
+window.onload = screen;
+
+function screen() {
+    mywidth = window.innerWidth;
+
+    document.getElementById("size").innerHTML = "Width : " + mywidth + "px";
+
+
+
+}
